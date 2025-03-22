b793cd12865d44e67c19761ab9182d48aa96877f
Arjav Jain <arjavjain0703@gmail.com>
2025-03-22 17:27:08 +0000

    add image URLs

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -3,8 +3,10 @@ import './App.css'
 
 export default function App() {
   return(
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    </>
   )
 }
