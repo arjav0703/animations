92a144f7648a3cc1eccacfd292268883764300de
Arjav Jain <arjavjain0703@gmail.com>
2025-03-22 20:07:44 +0000

    create different pages

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,14 +4,25 @@ import { Canvas } from './canvas'
 import data from './data'
 
 export default function App() {
+  const pages = [0,1,2]
   return(
     <>
-      <div className='bg-black text-white w-full min-h-screen absolute'>
-        
+      <div className='bg-[#FD2C2A] text-white w-full min-h-screen relative'>
         {data[0].map((canasdets, index) => (
-          <Canvas details={canasdets} key={index}/>
-       ))}
+            <Canvas details={canasdets} key={index}/>
+        ))}
       </div>
+      <div className='bg-[#FD2C2A] text-white w-full min-h-screen relative'>
+        {data[1].map((canasdets, index) => (
+            <Canvas details={canasdets} key={index}/>
+        ))}
+      </div>
+      <div className='bg-[#FD2C2A] text-white w-full min-h-screen relative'>
+        {data[2].map((canasdets, index) => (
+            <Canvas details={canasdets} key={index}/>
+        ))}
+      </div>
+      
     </>
   )
 }
