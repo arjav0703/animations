9fed97a42e1316a3c8de87220719f4cfb6edd4aa
Arjav Jain <arjavjain0703@gmail.com>
2025-03-22 18:34:04 +0000

    first animation

--- project/src/canvas.jsx
+++ project/src/canvas.jsx
@@ -1,20 +1,36 @@
 import { useEffect, useRef, useState } from "react";
 import images from './images'
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 export function Canvas() {
+  
+  const [index, setIndex] = useState({value: 0});
   const canvasRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(index, {
+      value: 149,
+      duration: 3,
+      ease: 'linear',
+      onUpdate: () => {
+        setIndex({value: Math.round(index.value)});
+      },
+      repeat: -1
+    })
+  })
   
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = new Image();
-    img.src = images[0];
+    img.src = images[index.value];
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0); 
     };
-  }, []);
+  }, [index]);
   return (
     <>
       <canvas
