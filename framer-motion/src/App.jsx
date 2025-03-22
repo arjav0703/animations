85e0484989d3accda8ac872bc3bca19956886af2
Arjav Jain <arjavjain0703@gmail.com>
2025-03-22 16:12:27 +0000

    drag prop is cool

--- framer-motion/src/App.jsx
+++ framer-motion/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'                                                  
 import './App.css'
-
+import { motion } from 'framer-motion'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <motion.div id='box' 
+        animate = {{
+          x: 700,
+          y:200
+        }}
+        transition={{
+          duration: 2,
+          delay: 0.2,
+          ease: 'anticipate',
+          pin:true,          
+
+        }}
+
+        >
+        </motion.div>
+
+        <motion.div drag id='dragbox'
+        // dragConstraints={{ left: 0, right: 1000, top: 0, bottom: 1000}}
+        whileDrag={{scale:0.8}}
+        ></motion.div>
+      </div>
     </>
   )
 }
