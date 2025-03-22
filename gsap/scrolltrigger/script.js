63707107c1d3e61157ce1009a49de9ff5e2cf1a3
Arjav Jain <arjavjain0703@gmail.com>
2025-03-22 10:51:13 +0000

    add scrollTrigger

--- gsap/scrolltrigger/script.js
+++ gsap/scrolltrigger/script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    gsap.to("#p1 #Box", {
+        rotate:60,
+        duration: 1,
+
+    });
+
+    gsap.to("#p2 #Box", {
+        rotate:60,
+        duration: 1,
+
+        scrollTrigger: {
+            trigger: "#p2 #Box",
+            start: "top center",
+            end: "bottom center",
+        }
+    });
 });
