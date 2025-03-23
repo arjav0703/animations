36c2372adaffaf6179acdb5f135a005330beb2a9
Arjav Jain <arjavjain0703@gmail.com>
2025-03-23 09:04:59 +0000

    integrate text and canvas

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -18,27 +18,28 @@ export default function App() {
   const pages = [0,1,2]
   return(
     <>
-      <script src="https://c.webfontfree.com/c.js?f=HelveticaNowDisplay-Regular" type="text/javascript"></script>
+      
       <div data-scroll-container id='main' >
-
-        {/* <div className='w-full min-h-screen relative'>
-          {data[0].map((canasdets, index) => (
-              <Canvas details={canasdets} key={index}/>
-          ))}
-        </div> */}
 
       <section className="w-full z-[1]">
         <MyNav />
-        <div className='w-[25%]  mx-[20%] leading-12 mt-10'>
+        <div className='w-[25%] top-[15%] mx-[20%] leading-12 mt-10 absolute z-99'>
           <h2 className='text-4xl'>
             At Thirtysixstudio, we build digital assets and immersive experiences for purposeful brands.
           </h2>
           <p className='text-md leading-4 mt-10 para'>
             We're a boutique production studio focused on design, animation, and technology, constantly rethinking what digital craft can do for present-day ads and campaigns.
           </p>
-          
         </div>
       </section>
+
+        <div className='w-full min-h-screen relative'>
+          {data[0].map((canasdets, index) => (
+              <Canvas details={canasdets} key={index}/>
+          ))}
+        </div>
+
+      
 
       </div>  
     </>
